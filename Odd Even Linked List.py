7d76ec71e2a7111581696ae5a431ad0f8f43d109
yashwini-reddy <151424956+yashwini-reddy@users.noreply.github.com>
2024-03-19 15:07:16 +0530

    Add files via upload

---
+++ Odd Even Linked List.py
@@ -0,0 +1,23 @@
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        stack=[]
+        begin=head
+        prev=head
+        if head:
+            head=head.next
+        else:
+            return head
+        while head:
+            tmp=head.next
+            head.next=None
+            stack.insert(0,head)
+            if tmp:
+                head=tmp.next
+                prev.next=tmp
+                prev=tmp
+            else:
+                head=tmp
+        while stack:
+            prev.next=stack.pop()
+            prev=prev.next
+        return begin
